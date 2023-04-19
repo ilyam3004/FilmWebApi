35953095749f67d9c00af11ef56382a21efaa8d8
ilyam3004 <ilyamelnichuk3004@gmail.com>
2023-04-20 00:00:38 +0300

    Configuring ApiGateway

--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -3,7 +3,8 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 
 var app = builder.Build();
 
