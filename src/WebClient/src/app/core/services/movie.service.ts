89c537fd93994df9e50718fd4dde0206a9319714
ilyam3004 <ilyamelnichuk3004@gmail.com>
2023-04-25 20:26:57 +0300

    Designing search page

--- src/WebClient/src/app/core/services/movie.service.ts
+++ src/WebClient/src/app/core/services/movie.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment.development";
+import { Observable } from 'rxjs';
+import { Movie } from '../models/movie';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +11,8 @@ export class MovieService {
 
   constructor(private httpClient: HttpClient) { }
 
-  searchMovie(query: string){
-    return this.httpClient.get(`
+  searchMovie(query: string): Observable<Movie[]>{
+    return this.httpClient.get<Movie[]>(`
         ${environment.apiBaseUrl}/movies/search/${query}`);
   }
 }
--- src/WebClient/src/app/modules/home/search/search.component.ts
+++ src/WebClient/src/app/modules/home/search/search.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MovieService} from "../../../core/services/movie.service";
-import {FormBuilder} from "@angular/forms";
+import {Movie} from "../../../core/models/movie";
+import { AlertService } from 'src/app/shared/services/alert.service';
+import { first } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -8,21 +10,22 @@ import {FormBuilder} from "@angular/forms";
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent {
-  movies: any;
-  form = this.formBuilder.group({
-    query: ''
-  });
+  inputValue: string = "";
+  movies: Movie[] = [];
 
   constructor(
     private movieService: MovieService,
-    private formBuilder: FormBuilder,) { }
+    private alertService: AlertService) { }
 
-  onSubmit():void {
-    if (this.form.value.query)
-    this.movieService.searchMovie(this.form.value.query)
-      .subscribe(response => {
-        this.movies = response;
-      });
-    console.log(this.movies);
+  processKeyup(query: string){
+    this.movieService.searchMovie(query)
+      .subscribe(
+        (response: Movie[]) => {
+          this.movies = response;
+        },
+        (error) => {
+          this.alertService.error(error);
+        }
+      );
   }
 }
